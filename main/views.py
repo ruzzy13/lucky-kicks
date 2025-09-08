7dc6d83277615b08c67f4cd5c28c924196b104c1
ruzzy13 <uzma.zhafira@gmail.com>
2025-09-08 19:54:53 +0700

    perbaiki error

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2406495451',
+        'name': 'Zhafira Uzma',
+        'class': 'PBP C'
+    }
+
+    return render(request, "main.html", context)
