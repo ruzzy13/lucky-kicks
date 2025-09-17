6e1db0765290b3ad29bc9ec34b65869d7f271af5
ruzzy13 <uzma.zhafira@gmail.com>
2025-09-17 09:02:56 +0700

    selesai menulis README.md

--- main/models.py
+++ main/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
     ]
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     name = models.CharField(max_length=255,default="air force")
     price = models.IntegerField(default=0)
     description = models.TextField()
