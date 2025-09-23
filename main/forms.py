d4cbb51b360fac80011cac14663f9786e46cf5de
ruzzy13 <uzma.zhafira@gmail.com>
2025-09-23 07:37:11 +0700

    challenge live coding pas demo

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
 from main.models import Product
+from django import forms
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ["name", "price", "description", "thumbnail", "category","is_featured", "stock"]
+
+class BolaForm(forms.Form):
+    name = forms.CharField(max_length=255)
+    brand = forms.CharField(max_length=255)
+    stock = forms.IntegerField(label = "Stok")
--- main/models.py
+++ main/models.py
@@ -1,14 +1,21 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
-    
+    CATEGORY_CHOICES = [
+        ('shoes', 'Shoes'),
+        ('apparel', 'Apparel'),
+        ('accessories', 'Accessories'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255,default="air force")
     price = models.IntegerField(default=0)
     description = models.TextField()
     thumbnail = models.URLField()
-    category = models.CharField(max_length=255,default="shoes")
+    category = models.CharField(max_length=255,choices=CATEGORY_CHOICES, default='Shoes')
     is_featured = models.BooleanField(default=False)
 
     # atribut sendiri
@@ -25,3 +32,11 @@ class Product(models.Model):
     def increment_views(self):
         self.views += 1
         self.save()
+
+class Bola(models.Model):
+    name = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
